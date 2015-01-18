d75dd1dddb40a717526129d168855bfb3e937170
Sean <pixelpicosean@gmail.com>
2015-01-18 10:22:26 +0800

    Cleanup outliner related code

--- app/controllers/dev.js
+++ app/controllers/dev.js
@@ -9,18 +9,5 @@ export default Ember.ObjectController.extend({
     },
     init: function() {
         this._super();
-
-        var family = Ember.EC.TreeNode.create({title: 'Family'})
-        // 1st level
-        var suz = family.createChild({title: 'Susan'})
-        var lud = family.createChild({title: 'Luda'})
-
-        // 2nd level
-        var josh = suz.createChild({title: 'Josh'})
-        var moses = suz.createChild({title: 'Moses'})
-        var verdi = lud.createChild({title: 'Verdi'})
-        var gaya = lud.createChild({title: 'Gaya'})
-
-        this.set('model', family);
     }
 });
