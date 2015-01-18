f764202e6389c88e973c1e3178aaa23c933a4bd1
Sean <pixelpicosean@gmail.com>
2015-01-18 11:25:07 +0800

    Remove outliner implementation from ember-idx-tree due to its complexity

--- app/controllers/dev.js
+++ app/controllers/dev.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-    actions: {
-        expand: function() {
-            this.get('selected').toggleProperty('expanded');
-            return null;
-        }
-    },
-    init: function() {
-        this._super();
-    }
 });
