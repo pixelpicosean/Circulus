44daff25bf4f3041918959ec8ee17e02bd0123d3
Sean <pixelpicosean@gmail.com>
2015-02-04 16:17:03 +0800

    Install and import PIXI.js

--- Brocfile.js
+++ Brocfile.js
@@ -18,5 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/mousetrap/mousetrap.js');
+app.import('bower_components/pixi/bin/pixi.dev.js');
 
 module.exports = app.toTree();
