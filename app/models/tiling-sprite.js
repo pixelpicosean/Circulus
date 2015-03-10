ae83f8765835293fe3e01042a547bd247e7dc5d3
Sean <pixelpicosean@gmail.com>
2015-03-10 18:36:01 +0800

    Change parent class of tiling-sprite to actor instead of model

--- app/models/tiling-sprite.js
+++ app/models/tiling-sprite.js
@@ -1,9 +1,7 @@
 import DS from 'ember-data';
 import Actor from './actor';
 
-export default DS.Model.extend({
+export default Actor.extend({
   image: DS.attr('string', { defaultValue: '' }),
-  width: DS.attr('number', { defaultValue: 1 }),
-  height: DS.attr('number', { defaultValue: 1 }),
   speed: DS.attr('vector', { defaultValue: { x: 0, y: 0 } })
 });
