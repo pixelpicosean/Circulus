55e143a543f9c36283ed31d5a5eeaa71d905f856
Sean <pixelpicosean@gmail.com>
2015-03-10 18:13:48 +0800

    Add children and parent relationships to actor

--- app/models/actor.js
+++ app/models/actor.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
-// import Vector from '../vector';
 
 export default DS.Model.extend({
+  children: DS.hasMany('actor', { polymorphic: true, inverse: 'parent' }),
+  parent: DS.belongsTo('actor', { polymorphic: true, inverse: 'children' }),
+
   anchor: DS.attr('vector', { defaultValue: { x: 0, y: 0 } }),
   rotation: DS.attr('number', { defaultValue: 0 }),
   size: DS.attr('vector', { defaultValue: { x: 1, y: 1 } }),
