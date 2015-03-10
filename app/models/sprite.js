5ab8b9a662b39424eb68538aa42cf544a56e82e2
Sean <pixelpicosean@gmail.com>
2015-03-10 18:13:29 +0800

    Add default value to sprite.image so that it won't be undefined

--- app/models/sprite.js
+++ app/models/sprite.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 import Actor from './actor';
 
 export default Actor.extend({
+  image: DS.attr('string', { defaultValue: '' }),
   flipX: DS.attr('boolean', { defaultValue: false }),
   flipY: DS.attr('boolean', { defaultValue: false }),
   alpha: DS.attr('number', { defaultValue: 1 })
