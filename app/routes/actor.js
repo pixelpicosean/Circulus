4b86fbbeae146786c67fccaba899fdc05876ebbf
Sean <pixelpicosean@gmail.com>
2015-03-15 12:38:15 +0800

    Modify test models

--- app/routes/actor.js
+++ app/routes/actor.js
@@ -19,18 +19,26 @@ export default Ember.Route.extend({
     parent.get('children').pushObject(child);
     child = this.store.push('tiling-sprite', {
       id: 3,
-      name: 'Ground',
-      image: 'background.png',
-      height: 32,
+      name: 'Parallel1',
+      image: 'parallax1.png',
+      size: {
+        x: 1280,
+        y: 376
+      },
       speed: {
         x: 10, y: 0
+      },
+      position: {
+        x: 0, y: 400
       }
     });
     parent.get('children').pushObject(child);
     child = this.store.push('animation', {
       id: 4,
-      spritesheet: 'coin.png',
-      name: 'Coin'
+      name: 'Player',
+      spritesheet: 'player1.png',
+      speed: 0.2,
+      loop: true
     });
     parent.get('children').pushObject(child);
 
