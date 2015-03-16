519a7c655e3eb67abf2598897a69d7c770d568ff
Sean <pixelpicosean@gmail.com>
2015-03-16 22:19:52 +0800

    Better actor property group in actor-inspector

--- app/models/actor.js
+++ app/models/actor.js
@@ -5,11 +5,11 @@ export default DS.Model.extend({
   children: DS.hasMany('actor', { polymorphic: true, inverse: 'parent' }),
   parent: DS.belongsTo('actor', { polymorphic: true, inverse: 'children' }),
 
-  alpha: DS.attr('number', { defaultValue: 1 }),
+  name: DS.attr('string', { defaultValue: '' }),
+  position: DS.attr('vector', { defaultValue: { x: 0, y: 0 } }),
   rotation: DS.attr('number', { defaultValue: 0 }),
   scale: DS.attr('vector', { defaultValue: { x: 1, y: 1 } }),
-  position: DS.attr('vector', { defaultValue: { x: 0, y: 0 } }),
-  name: DS.attr('string', { defaultValue: '' }),
+  alpha: DS.attr('number', { defaultValue: 1 }),
 
   // For tree component display
   title: Ember.computed.alias('name'),
--- app/models/animation.js
+++ app/models/animation.js
@@ -2,16 +2,16 @@ import DS from 'ember-data';
 import Actor from './actor';
 
 export default Actor.extend({
+  anchor: DS.attr('vector', { defaultValue: { x: 0, y: 0 } }),
+
+  speed: DS.attr('number', { defaultValue: 1 }),
+  loop: DS.attr('boolean', { defaultValue: true }),
+  frames: DS.attr('array'),
+
   useSpritesheet: DS.attr('boolean', { defaultValue: false }),
   spritesheet: DS.attr('string', { defaultValue: '' }),
   frameWidth: DS.attr('number', { defaultValue: 16 }),
   frameHeight: DS.attr('number', { defaultValue: 16 }),
-
-  frames: DS.attr('array'),
-  speed: DS.attr('number', { defaultValue: 1 }),
-  loop: DS.attr('boolean', { defaultValue: true }),
-
-  anchor: DS.attr('vector', { defaultValue: { x: 0, y: 0 } }),
 
   nodeType: 'animation',
   isAnimation: true
