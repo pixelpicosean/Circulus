33d69ec9470450d1db155f7d7f857946a96fe72c
Sean Bohan <pixelpicosean@gmail.com>
2015-04-04 10:44:28 +0800

    Fix Content Security Policy issue.

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,10 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.contentSecurityPolicy = {
+      'font-src': "'self' *",
+      'style-src': "'self' *"
+    };
   }
 
   if (environment === 'test') {
