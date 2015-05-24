9c3d075863dec7c316fef02e6a12358b0f80a5ba
Sean Bohan <pixelpicosean@gmail.com>
2015-05-24 16:43:49 +0800

    Remove old styles.

--- Brocfile.js
+++ Brocfile.js
@@ -10,16 +10,6 @@ app.import('bower_components/ember-localstorage-adapter/localstorage_adapter.js'
 // Mousetrap
 app.import('bower_components/mousetrap/mousetrap.js');
 
-// Font Awesome
-app.import('bower_components/font-awesome/font-awesome.scss');
-
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', { destDir: 'fonts' });
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', { destDir: 'fonts' });
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', { destDir: 'fonts' });
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', { destDir: 'fonts' });
-app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2', { destDir: 'fonts' });
-app.import('bower_components/font-awesome/fonts/FontAwesome.otf', { destDir: 'fonts' });
-
 // Pixi.js as Renderer
 app.import('bower_components/pixi.js/bin/pixi.dev.js');
 
