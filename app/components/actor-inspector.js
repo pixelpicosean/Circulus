9e10bcb816a24d21ae32b6c691fe824f8c96f9cb
Sean Bohan <pixelpicosean@gmail.com>
2015-05-25 11:34:49 +0800

    Restyle inspector.

--- app/components/actor-inspector.js
+++ app/components/actor-inspector.js
@@ -3,7 +3,7 @@ import layout from '../templates/components/actor-inspector';
 
 export default Ember.Component.extend({
   layout: layout,
-  classNames: ['actor-inspector'],
+  classNames: [''],
 
   editable: true,
 
